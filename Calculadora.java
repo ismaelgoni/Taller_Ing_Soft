4cb7bf043854f2872edddca030602cfefec57ae1
alumnoFI <alumnoFI@facultades.ort.edu.uy>
2017-05-24 20:47:53 -0300

    Agrego Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       if(args.length==3){
+       try{
+           int op1 = Integer.parseInt(args[1]);
+           int op2 = Integer.parseInt(args[2]);
+           
+           System.out.println(op1 + op2);
+       }catch(Exception ex){
+           System.out.println("Parametros Incorrectos");
+       }
+       }else{
+           System.out.println("Uso: Calculadora operador operando1 operando2");
+       }
+    }
+    
+}
